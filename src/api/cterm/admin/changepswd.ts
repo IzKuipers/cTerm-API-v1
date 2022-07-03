baf1cc2513283dd1458e99068f22471ffac957f2
TechWorldInc <izaak.kuipers@gmail.com>
2022-07-04 00:20:16 +0200

    Fix admin/changepswd and re-include src/db/main.ts

--- src/api/cterm/admin/changepswd.ts
+++ src/api/cterm/admin/changepswd.ts
@@ -5,18 +5,18 @@ import { commitChanges, getDB, setDB } from "../../../db/main";
 import { createErrorRes, writeToRes } from "../../../server/return";
 import argon2 from "argon2";
 import { userExists } from "../../../auth/user";
+import { getAuth } from "../../../auth/get";
 export async function adminChangePswd(
   req: IncomingMessage,
   res: ServerResponse
 ) {
+  const {username} = getAuth(req);
   const query = url.parse(req.url as string, true).query;
-
-  const username = atob(query["user"] as string);
+  const targetUser = atob(query["user"] as string);
   const newPsswd = atob(query["new"] as string);
-
   const cdb = (await getDB("cred")) as { [key: string]: string };
 
-  if (!(await userExists(username))) {
+  if (!(await userExists(targetUser))) {
     res.statusCode = 406;
     writeToRes(
       res,
@@ -28,7 +28,7 @@ export async function adminChangePswd(
     return;
   }
 
-  if (await isAdmin(username)) {
+  if (await isAdmin(targetUser) && username != targetUser) {
     res.statusCode = 405;
     writeToRes(
       res,
@@ -47,7 +47,7 @@ export async function adminChangePswd(
     hashLength: 32,
   });
 
-  cdb[username] = pswd;
+  cdb[targetUser] = pswd;
 
   commitChanges("change password", res, { db: "cred", data: cdb });
 }
