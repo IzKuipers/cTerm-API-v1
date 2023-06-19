16c5c848432bc5129c781abd4cf8c86e5a539cf9
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-06-19 14:30:21 +0200

    Change port

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { cTermAPIEval } from "./server/endpoint/store";
 import { makeServer } from "./server/main";
 
-makeServer(3000, "cTerm", cTermAPIEval);
+makeServer(4567, "cTerm", cTermAPIEval);
